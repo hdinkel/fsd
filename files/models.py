55c2d457b19c0ab7e41ff9a44d4d01d90d39e370
Holger Dinkel <Holger.dinkel@leibniz-fli.de>
2017-12-06 10:27:18 +0100

    fix models, uploads now work again

--- files/models.py
+++ files/models.py
@@ -4,25 +4,28 @@ import hashlib
 # Create your models here.
 
 class File(models.Model):
-    name = models.CharField(max_length=200, primary_key=True, unique=True)
-    hash = models.CharField(max_length=200, unique=True)
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, blank=True, unique=False)
+    hash = models.CharField(max_length=200, unique=True, editable=False)
     file = models.FileField() # TODO: add something like this: upload_to='documents/%Y/'); also use username?
-    filesize = models.IntegerField(blank = True, null = True, editable = False)
-    created_at = models.DateTimeField(auto_now_add=True, editable=False)
-    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    filesize = models.IntegerField(blank=True, null=True, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def save(self, *args, **kwargs):
         super(File, self).save(*args, **kwargs)
         self.filesize = self.file.size
-        f = self.file.open('rb')
-        hash = hashlib.sha256()
-        if f.multiple_chunks():
-           for chunk in f.chunks():
-              hash.update(chunk)
-        else:
-              hash.update(f.read())
-        f.close()
+        with self.file.open('rb') as f:
+            hash = hashlib.sha256()
+            if f.multiple_chunks():
+                for chunk in f.chunks():
+                    hash.update(chunk)
+                else:
+                    hash.update(f.read())
         self.hash =  hash.hexdigest()
+        if self.name  == '':
+#            self.name = f.name
+            self.name = self.file.name
         super(File, self).save(*args, **kwargs)
 
     def __str__(self):
