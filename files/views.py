7f3a2789955f1b3a4c56bf93041a309bce05f140
Holger Dinkel <Holger.dinkel@leibniz-fli.de>
2017-12-14 14:44:30 +0100

    add templates for files

--- files/views.py
+++ files/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import View, DetailView, TemplateView, ListView
+from .models import File
 
 # Create your views here.
 
 
+class FileDetailView(DetailView):
+    template_name = 'files/file_detail.html'
+    model = File
+
+class FileList(ListView):
+    model = File
