0aa649106490b404b793c8557bdcae3d982da9a7
Holger Dinkel <Holger.dinkel@leibniz-fli.de>
2017-12-15 10:23:26 +0100

    update base.html to include css

--- fsd/settings.py
+++ fsd/settings.py
@@ -55,7 +55,6 @@ ROOT_URLCONF = 'fsd.urls'
 MEDIA_URL = '/uploads/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
-STATIC_ROOT = 'static'
 
 TEMPLATES = [
     {
@@ -123,4 +122,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
