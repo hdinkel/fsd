cfb05fbe9d5d99298810fcc701b2ca062244daf5
Holger Dinkel <Holger.dinkel@leibniz-fli.de>
2018-01-02 11:35:49 +0100

    BUGFIX: project info does not get saved (always default project)

--- files/utils.py
+++ files/utils.py
@@ -2,6 +2,9 @@ import zipfile
 import tempfile
 import os
 import shutil
+# from django.contrib.admin.models import LogEntry, ADDITION
+# from django.contrib.contenttypes.models import ContentType
+# from django.utils.encoding import force_text
 from fsd.settings import MEDIA_ROOT
 from .models import File
 from .models import create_hash
@@ -13,8 +16,6 @@ def extract_zipfile(filename, username, project):
     and generate a Files object of each file.
     """
     z = zipfile.ZipFile(filename)
-#    print("Username:", username)
-#    print("project:", project)
     with tempfile.TemporaryDirectory(dir=MEDIA_ROOT) as tmpdirname:
         print("TmpDirName:", tmpdirname)
         for file in z.namelist():
@@ -33,13 +34,24 @@ def extract_zipfile(filename, username, project):
                         print("File {} with the following hash already exists in DB: '{}'".format(f.id, hash))
                     except File.DoesNotExist:
                         print('file:', file)
-                        f = File(file=tmpfilename, name=file, comment = "bundled upload via zipfile '{}'".format(filename))
+                        f = File(file=tmpfilename, name=file, project=project, comment = "bundled upload via zipfile '{}'".format(filename))
                         f.save()
+
+# Throws error:
+#                        LogEntry.objects.log_action(
+#                            user_id         = username,
+#                            content_type_id = ContentType.objects.get_for_model(f).pk,
+#                            object_id       = f,
+#                            object_repr     = force_text(f),
+#                            action_flag     = ADDITION
+#                        )
+
                         dstdir = os.path.dirname(tmpfilename.replace(tmpdirname, MEDIA_ROOT))
                         print(dstdir)
                         if not os.path.isdir(dstdir):
                             os.makedirs(dstdir)
                         shutil.copy2(tmpfilename, dstdir)
+                        # TODO: file = dstdir
 #                except Exception:
                     # TODO catch hash collisions (also empty files produce collisions)
 #                    pass
